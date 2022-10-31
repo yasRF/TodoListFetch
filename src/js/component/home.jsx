9798554857aab831ae55d64da7d1cc95ad037173
yasRF <yas070788@hotmail.es>
2022-10-31 15:09:22 +0000

    mi primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,69 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		const [newItem, setNewItem] = useState("");
+		const [items, setItems] = useState([]);
+
+		//fetch
+		useEffect(() => {
+		  
+		  fetch("https://assets.breatheco.de/apis/fake/todos/user/alesanchezr")
+			.then(response => response.json())
+			.then(result => {
+				setNewItem(result)
+				console.log(result)})
+			.catch(error => console.log('error', error));
+			}, []);
+
+			/*/response
+			[
+    {
+        "label": "sample task",
+        "done": false
+    },
+    {
+        "label": "ggfgdgfd",
+        "id": "ebd0e30f-c5ac-4813-a69c-19aeed9b597e",
+        "done": false
+    },
+    {
+        "label": "fsdsf",
+        "id": "665aabab-1f17-435e-8c01-b54ffb0853dc",
+        "done": true
+    }
+] /*/
+		function addItem() {
+		const item= {
+			id: Math.floor(Math.random() * 1000),
+			value: newItem
+		};
+		setItems(oldList => [...oldList, item]);
+		setNewItem("");
+	 }
+	 function deleteItem(id){
+		const newArray = items.filter(item => item.id !==id);
+		setItems(newArray);
+	
+	 };
+
+	return(
+		<div className="App">
+			<div className="text-center">
+			<h1>Lista de tareas</h1>
+			<input type="text" placeholder='Add an item...' value={newItem}
+			onChange={e => setNewItem(e.target.value)}
+			/>
+			<button onClick={() => addItem()}>Crear</button>
+			<ul>
+				{items.map(item => {
+					return(
+						<li key={item.id}>{item.value}<button onClick={() => deleteItem(item.id)}>Eliminar</button></li>
+					)
+				})}
+			</ul>
 		</div>
-	);
+		</div>
+	)
 };
 
 export default Home;
